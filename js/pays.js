64f03c1cc989dfd9431cdb6df98ddd73035b896c
Xavier Arbour <119866415+IshtarFolo@users.noreply.github.com>
2024-05-31 16:53:54 -0400

    epreuve-finale correction de l'url, adaptation du style du menu sur ecrans mobiles et travail sur l'affichage du contenu de la REST-API

--- js/pays.js
+++ js/pays.js
@@ -1,10 +1,8 @@
 function fetchEtAffichePays(paysChoisi) {
-    let url = "http://localhost/wordpress/wp-json/wp/v2/pays?_embed&per_page=57"; // URL du serveur local
- // let url = `http://localhost:8085/4w4-2024-gr1/wp-json/wp/v2/pays?per_page=57`; // URL du serveur local
+  let url = `http://localhost:8085/4w4-2024-gr1/wp-json/wp/v2/posts?_embed&per_page=57`; // URL du serveur local
 
 // fetch pour recuperer les donnees associees a l'URL
 fetch(url)
-
 .then(function (response) {
     // Check si la reponse est Ok (HTTP status 200)
     if (!response.ok) {
@@ -44,12 +42,23 @@ fetch(url)
         carte.innerHTML = `
         <h2>${titre}</h2>
         <p>${contenu}</p><br>
-        // On affiche l'image de la destination et si elle ne contient pas d'image on met l'adresse https://via.placeholder.com/150 comme placeholder
-        <img src="${article.acf.image ? article.acf.image : (article._embedded['wp:featuredmedia'][0].source_url ? article._embedded['wp:featuredmedia'][0].source_url : 'https://via.placeholder.com/150')}" alt="Image de la destination" style="width: 150px; height: 150px;">
-        `;
+        //  On affiche l'image de la destination et si elle ne contient pas d'image on met l'adresse https://via.placeholder.com/150 comme placeholder
+
+        <img src="${article.acf.image ? article.acf.image : (article._embedded['wp:featuredmedia'][0].source_url ? article._embedded['wp:featuredmedia'][0].source_url : 
+        'https://via.placeholder.com/150')}" alt="Image de la destination" style="width: 150px; height: 150px;">`;
         restapi.appendChild(carte);
     })
 })
+}
 
+// Ajout d'un ecouteur d'evenements sur les liens du menu
+window.onload = function() {
+    document.querySelectorAll('.item-menu').forEach(function(link) {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
 
+            let paysChoisi = this.textContent;
+            fetchEtAffichePays(paysChoisi);
+        });
+    });
 }
